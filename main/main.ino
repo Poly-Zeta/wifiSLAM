8398b1efa4fdb879cb8f29aae5742dc094c757a3
Poly-Zeta <55525642+Poly-Zeta@users.noreply.github.com>
2025-01-29 08:21:44 +0900

    各デバイスの導通を実装，起動時にフラグ見て動くように

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,11 @@
 #include "Wire.h"
 #include <Arduino.h>
+#include "math.h"
 
 //TODO:各I2Cデバイスがアクティブかどうかの管理フラグの作成
 //->Init時にアクティブだった場合のみ，IOを実施(そのぶんのifぐらいはクロックが間に合うはず)
 //特にモタドラとBlinkは2層目基板にあるため外せるようにしておく
+//どっかでセンサ番号からセンサのI2Caddrを取得できるようにしておいたほうがいい
 
 #define MAINLOOP_CYCLE_MS 10
 
@@ -85,13 +87,13 @@ enum SensorsBufferNUM{
 };
 
 enum I2CSensorsNUM{
-  IMU_BNO055,
-  DISP_SSD1306,
-  HUM_BME280,
-  WHEELS,
-  LED_BlinkM,
-  ADC_ADS1115,
-  SERVO_PCA9685
+  IMU_BNO055,   //00
+  DISP_SSD1306, //01
+  HUM_BME280,   //02
+  WHEELS,       //03
+  LED_BlinkM,   //04
+  ADC_ADS1115,  //05
+  SERVO_PCA9685 //06
 };
 
 //ディスプレイ描画バッファ
@@ -169,8 +171,29 @@ const uint8_t fonts[FONTDATA_SIZE][SSD1306_CHARLINEDATA_SIZE]={
   {0x00,0xC6,0xC6,0xE6,0xD6,0xCE,0xC6,0xC6}// Z 5A
 };
 
+//導通チェックしてフラグ書き換え
+void I2CConnectChk(int sensorAddr,int sensorNum){
+  byte error;
+  Wire.beginTransmission(sensorAddr);
+  error = Wire.endTransmission();
+  if(error==0){
+    I2CActiveFlags=I2CActiveFlags | (1<<sensorNum);
+  }
+  return;
+}
+
+bool I2CConnectFlgChk(int sensorNum){
+  bool flg;
+  flg=(I2CActiveFlags>>sensorNum) & 0x01;
+  
+  return flg;
+}
+
 //センサデータの描画　毎回全更新してると遅いのでメイン1ループあたり1要素
 void SSD1306_displaySensorsData(){
+  if(!I2CConnectFlgChk(DISP_SSD1306)){
+    return;
+  }
   static uint8_t cnt=SENSORS_DISPLAY_OFFSET;
 
   uint8_t updatePage,updateChar,updateSize,dtostrfSize;
@@ -247,6 +270,9 @@ void SSD1306_displaySensorsData(){
 void SSD1306_display1LineWithShiftUp(char* input){
   static uint8_t updatePage=0;//0,1,...7と更新していく．8になったら以降はずっとシフトアップ+7を更新
   // Serial.println(input);
+  if(!I2CConnectFlgChk(DISP_SSD1306)){
+    return;
+  }
 
   if(updatePage>=SSD1306_PAGES_SIZE){//全ページ更新済なので，バッファシフトアップして7ページ目更新
 
@@ -352,6 +378,9 @@ void SSD1306_display1LineWithShiftUp(char* input){
 void SSD1306_FullFillSample(){
   int fontCount=0;
   uint8_t chara,page,cntByteLine;
+  if(!I2CConnectFlgChk(DISP_SSD1306)){
+    return;
+  }
 
   for(page=0;page<SSD1306_PAGES_SIZE;page++){//全ページを対象
 
@@ -390,6 +419,11 @@ void SSD1306_FullFillSample(){
 
 //OLED SSD1306 初期化
 void SSD1306_Init(){
+  I2CConnectChk(ADDRESS_SSD1306,DISP_SSD1306);
+  if(!I2CConnectFlgChk(DISP_SSD1306)){
+    return;
+  }
+
   Wire.beginTransmission(ADDRESS_SSD1306);//※このバイトも含め、以後、合計32byteまで送信できる
 
   Wire.write(0b10000000); //control byte, Co bit = 1, D/C# = 0 (command)
@@ -463,6 +497,9 @@ void SSD1306_Init(){
 //OLED SSD1306 表示全クリア
 void SSD1306_ClearAll(){
   uint8_t i, j, k;
+  if(!I2CConnectFlgChk(DISP_SSD1306)){
+    return;
+  }
   
   for(i = 0; i < SSD1306_PAGES_SIZE; i++){//Page(0-7)
     Wire.beginTransmission(ADDRESS_SSD1306);
@@ -488,6 +525,9 @@ void SSD1306_ClearAll(){
 
 //IMU BNO055 コンフィグ用write関数
 void BNO055_Write(byte reg, byte value, int delayMs){
+  if(!I2CConnectFlgChk(IMU_BNO055)){
+    return;
+  }
   Wire.beginTransmission(ADDRESS_BNO055);
   Wire.write(reg);
   Wire.write(value);
@@ -499,6 +539,9 @@ void BNO055_Write(byte reg, byte value, int delayMs){
 //IMU BNO055 値の読み出し
 void BNO055_getRawData(){
   uint8_t buffer[8];
+  if(!I2CConnectFlgChk(IMU_BNO055)){
+    return;
+  }
 
   Wire.beginTransmission(ADDRESS_BNO055);  
     Wire.write(REG_BNO055_GYRO);
@@ -551,6 +594,10 @@ void BNO055_getRawData(){
 
 //IMU BNO055 初期化
 void BNO055_Init(){
+  I2CConnectChk(ADDRESS_BNO055,IMU_BNO055);
+  if(!I2CConnectFlgChk(IMU_BNO055)){
+    return;
+  }
   Wire.beginTransmission(ADDRESS_BNO055);
   Wire.write(0x00);
   Wire.endTransmission();
@@ -584,6 +631,10 @@ void BNO055_Calibration(){
   const uint8_t REG_BNO055_CALIBRATED=0x35;
 
   buffer[0] = 0b00000000;
+
+  if(!I2CConnectFlgChk(IMU_BNO055)){
+    return;
+  }
 
   SSD1306_display1LineWithShiftUp("CALIBRATION...");
   SSD1306_display1LineWithShiftUp("GYRO 0 ACC 0");
@@ -667,6 +718,11 @@ void BNO055_Calibration(){
 
 //温度センサ BME280 初期化
 void BME280_Init(){
+  I2CConnectChk(ADDRESS_BME280,HUM_BME280);
+  if(!I2CConnectFlgChk(HUM_BME280)){
+    return;
+  }
+  
   Wire.beginTransmission(ADDRESS_BME280);
   Wire.write(0xF2);
   Wire.write(0x01);
@@ -688,6 +744,10 @@ void BME280_Init(){
 
 void BME280_readTrim(){
   uint8_t data[32],i=0;                      // Fix 2014/04/06
+  if(!I2CConnectFlgChk(HUM_BME280)){
+    return;
+  }
+  
   Wire.beginTransmission(ADDRESS_BME280);
   Wire.write(0x88);
   Wire.endTransmission();
@@ -793,6 +853,10 @@ void BME280_getRawData(){
   signed long int temp_cal;
   unsigned long int press_cal,hum_cal;
 
+  if(!I2CConnectFlgChk(HUM_BME280)){
+    return;
+  }
+
 //   気圧の情報はレジスタのアドレス　0xF7 0xF8 0xF9に格納されます。
 // 温度の情報はレジスタのアドレス　0xFA 0xFB 0xFCに格納されます。
 // 湿度の情報はレジスタのアドレス　0xFD 0xFE に格納されます。　湿度だけ2byteです。
@@ -843,59 +907,67 @@ void BME280_getRawData(){
   return;
 }
 
+//float型のデータを1Byteずつ送信
+void SerialOutputFloat(float data){
+  for(int i=0;i<4;i++){
+    Serial.print(((uint8_t*)&data)[i]);
+  }
+  return;
+}
+
 //センサ値のシリアル出力
 void SerialOutput(){
   //起動後時間，角速度，磁気，加速度，線形加速度，四元数，(温度，湿度，気圧，左モータ回転，右モータ回転)
   Serial.print(millis());
   Serial.print(",");
 
-  Serial.print(sensorsDataBuffer[GYRO_X]);
+  SerialOutputFloat(sensorsDataBuffer[GYRO_X]);
   Serial.print(",");
-  Serial.print(sensorsDataBuffer[GYRO_Y]);
+  SerialOutputFloat(sensorsDataBuffer[GYRO_Y]);
   Serial.print(",");
-  Serial.print(sensorsDataBuffer[GYRO_Z]);
-  Serial.print(",");
-  
-  Serial.print(sensorsDataBuffer[MAG_X]);
-  Serial.print(",");
-  Serial.print(sensorsDataBuffer[MAG_Y]);
-  Serial.print(",");
-  Serial.print(sensorsDataBuffer[MAG_Z]);
+  SerialOutputFloat(sensorsDataBuffer[GYRO_Z]);
   Serial.print(",");
   
-  Serial.print(sensorsDataBuffer[ACC_X]);
+  SerialOutputFloat(sensorsDataBuffer[MAG_X]);
   Serial.print(",");
-  Serial.print(sensorsDataBuffer[ACC_Y]);
+  SerialOutputFloat(sensorsDataBuffer[MAG_Y]);
   Serial.print(",");
-  Serial.print(sensorsDataBuffer[ACC_Z]);
-  Serial.print(",");
-  
-  Serial.print(sensorsDataBuffer[LACC_X]);
-  Serial.print(",");
-  Serial.print(sensorsDataBuffer[LACC_Y]);
-  Serial.print(",");
-  Serial.print(sensorsDataBuffer[LACC_Z]);
+  SerialOutputFloat(sensorsDataBuffer[MAG_Z]);
   Serial.print(",");
   
-  Serial.print(sensorsDataBuffer[QW]);
+  SerialOutputFloat(sensorsDataBuffer[ACC_X]);
   Serial.print(",");
-  Serial.print(sensorsDataBuffer[QX]);
+  SerialOutputFloat(sensorsDataBuffer[ACC_Y]);
   Serial.print(",");
-  Serial.print(sensorsDataBuffer[QY]);
-  Serial.print(",");
-  Serial.print(sensorsDataBuffer[QZ]);
+  SerialOutputFloat(sensorsDataBuffer[ACC_Z]);
   Serial.print(",");
   
-  Serial.print(sensorsDataBuffer[TEMP]);
+  SerialOutputFloat(sensorsDataBuffer[LACC_X]);
   Serial.print(",");
-  Serial.print(sensorsDataBuffer[HUMID]);
+  SerialOutputFloat(sensorsDataBuffer[LACC_Y]);
   Serial.print(",");
-  Serial.print(sensorsDataBuffer[PRESS]);
+  SerialOutputFloat(sensorsDataBuffer[LACC_Z]);
+  Serial.print(",");
+  
+  SerialOutputFloat(sensorsDataBuffer[QW]);
+  Serial.print(",");
+  SerialOutputFloat(sensorsDataBuffer[QX]);
+  Serial.print(",");
+  SerialOutputFloat(sensorsDataBuffer[QY]);
+  Serial.print(",");
+  SerialOutputFloat(sensorsDataBuffer[QZ]);
+  Serial.print(",");
+  
+  SerialOutputFloat(sensorsDataBuffer[TEMP]);
+  Serial.print(",");
+  SerialOutputFloat(sensorsDataBuffer[HUMID]);
+  Serial.print(",");
+  SerialOutputFloat(sensorsDataBuffer[PRESS]);
   Serial.print(",");
 
-  Serial.print(sensorsDataBuffer[L_WHEEL]);
+  SerialOutputFloat(sensorsDataBuffer[L_WHEEL]);
   Serial.print(",");
-  Serial.print(sensorsDataBuffer[R_WHEEL]);
+  SerialOutputFloat(sensorsDataBuffer[R_WHEEL]);
 
   // Serial.print(",");
 
@@ -906,6 +978,11 @@ void SerialOutput(){
 }
 
 void MotorDriver_Init(){
+  I2CConnectChk(ADDRESS_WHEELS,WHEELS);
+  if(!I2CConnectFlgChk(WHEELS)){
+    return;
+  }
+  
   Wire.beginTransmission(ADDRESS_WHEELS);
   Wire.write(MOTOR_TYPE_ADDR);
   Wire.write(MOTOR_TYPE_JGB37_520_12V_110RPM);
@@ -929,6 +1006,10 @@ void MotorDriver_Init(){
 }
 
 void WriteMotors(int8_t l_wheelPower,int8_t r_wheelPower){
+  if(!I2CConnectFlgChk(WHEELS)){
+    return;
+  }
+  
   Wire.beginTransmission(ADDRESS_WHEELS);
   Wire.write(MOTOR_FIXED_SPEED_ADDR);
   Wire.write(l_wheelPower);
@@ -941,6 +1022,10 @@ void WriteMotors(int8_t l_wheelPower,int8_t r_wheelPower){
 
 void readEncoders(){
   uint32_t buffer[2];
+
+  if(!I2CConnectFlgChk(WHEELS)){
+    return;
+  }
 
   Wire.beginTransmission(ADDRESS_WHEELS);
   Wire.write(MOTOR_ENCODER_TOTAL_ADDR);
@@ -960,16 +1045,20 @@ void setup() {
   Wire.begin();
   Wire.setClock(100000L);
   delay(10);
+  byte error;
 
   //BlinkMの光を弱くしておく
-  Wire.beginTransmission(ADDRESS_BlinkM);
-  Wire.write(0x6f);
-  Wire.write(0x6e);
-  Wire.write(0x04);
-  Wire.write(0x04);
-  Wire.write(0x04);
-  Wire.endTransmission();
-
+  I2CConnectChk(ADDRESS_BlinkM,LED_BlinkM);
+  if(I2CConnectFlgChk(LED_BlinkM)){
+    Wire.beginTransmission(ADDRESS_BlinkM);
+    Wire.write(0x6f);
+    Wire.write(0x6e);
+    Wire.write(0x04);
+    Wire.write(0x04);
+    Wire.write(0x04);
+    Wire.endTransmission();
+  }
+  
   SSD1306_Init(); //OLED ssd1306 初期化
   
   SSD1306_ClearAll();
@@ -1000,7 +1089,7 @@ void setup() {
 
   SSD1306_display1LineWithShiftUp("BNO055 SETUP");//IMU BNO055初期化
   BNO055_Init();
-  BNO055_Calibration();
+  // BNO055_Calibration();
   SSD1306_display1LineWithShiftUp("BNO055 STANDBY");
 
   SSD1306_display1LineWithShiftUp("SETUP COMPLETE");
